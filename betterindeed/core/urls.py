2cc5c1f2a1e5d77783c811dff555b67a4b4a9f57
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2023-08-19 13:08:43 -0700

    user profile added

--- betterindeed/core/urls.py
+++ betterindeed/core/urls.py
@@ -6,5 +6,6 @@ app_name = 'core'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register')
+    path('register/', views.register_view, name='register'),
+    path('profile/', views.profile, name='profile')
 ]
--- betterindeed/core/views.py
+++ betterindeed/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from .forms import UserRegisterForm
+from .models import Profile
 
 
 def register_view(request):
@@ -8,6 +10,9 @@ def register_view(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            user = User.objects.filter(username=form.data['username']).first()
+            profile = Profile(user=user)
+            profile.save()
             return redirect('/login')
         else:
             return render(request, 'core/register.html', {'form': form})
@@ -33,3 +38,7 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+
+def profile(request):
+    return render(request, 'core/profile.html')
